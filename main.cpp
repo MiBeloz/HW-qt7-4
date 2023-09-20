36f5083244afb15c75dfa2970cd821f620b624ff
MiBeloz <miha_bmv_@mail.ru>
2023-09-21 00:45:07 +0600

    final

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     w.setWindowTitle("Домашнее задание №4");
-    w.setMinimumSize(330, 230);
+    w.setMinimumSize(330, 260);
     w.setMaximumSize(600, 500);
 
     w.show();
